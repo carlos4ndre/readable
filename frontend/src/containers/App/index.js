5d0447fd897bf5d0a7ad48cc3c874c05bfe664f5
Carlos André <carlos.ulrich@gmail.com>
2017-11-03 11:43:55 +0000

    Decouple header from other views

--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Switch, BrowserRouter, Route } from 'react-router-dom'
+import { getCategories } from 'actions/categories'
+import { Divider } from 'semantic-ui-react'
+import Header from 'components/Header'
 import HomePage from 'containers/HomePage'
 import PostPage from 'containers/PostPage'
 import CategoryPage from 'containers/CategoryPage'
 
 class App extends Component {
+  componentWillMount() {
+    this.props.dispatch(getCategories())
+  }
+
   render() {
+    const { categories } = this.props
+
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/categories/:categoryId" component={CategoryPage}/>
-          <Route path="/posts/:postId" component={PostPage}/>
-        </Switch>
+        <div>
+          <Header categories={categories}/>
+          <Divider hidden/>
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/categories/:categoryId" component={CategoryPage}/>
+            <Route path="/posts/:postId" component={PostPage}/>
+          </Switch>
+        </div>
       </BrowserRouter>
     )
   }
 }
 
-export default App
+App.PropTypes = {
+  dispatch: PropTypes.func,
+}
+
+const mapStateToProps = (state) => {
+  const categories = state.categories
+
+  return {
+    categories,
+  }
+}
+
+export default connect(mapStateToProps)(App)
--- frontend/src/containers/HomePage/index.js
+++ frontend/src/containers/HomePage/index.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Divider } from 'semantic-ui-react'
-import { getCategories } from 'actions/categories'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { getPosts } from 'actions/posts'
-import Header from 'components/Header'
 import PostList from 'components/PostList'
 import SortBy from 'components/SortBy'
 
 class HomePage extends Component {
   componentWillMount() {
-    this.props.dispatch(getCategories())
     this.props.dispatch(getPosts())
   }
 
   render() {
-    const { categories, posts } = this.props
+    const { posts } = this.props
 
     return (
       <div>
-        <Header categories={categories}/>
-        <Divider hidden/>
         <SortBy />
         <PostList posts={posts}/>
       </div>
@@ -33,12 +27,9 @@ HomePage.PropTypes = {
 }
 
 const mapStateToProps = (state) => {
-  const categories = state.categories
   const posts = state.posts
-  console.log(state)
 
   return {
-    categories,
     posts,
   }
 }
