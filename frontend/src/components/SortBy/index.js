c5468b6e331fdfef781e18fac5a9d73ad0aeb31a
Carlos André <carlos.ulrich@gmail.com>
2017-11-13 12:54:38 +0000

    Sort by comments

--- frontend/src/components/SortBy/index.js
+++ frontend/src/components/SortBy/index.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Menu, Dropdown, Container  } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
 import { OPTIONS, TOP } from 'utils/sorting'
 
 const SortBy = (props) => (
-  <Container>
-    <Menu compact>
-      <Dropdown
-        selection
-        compact
-        text='SortBy'
-        options={OPTIONS}
-        defaultValue={TOP}
-        onChange={(event, data) => props.onChange(data.value)}
-      />
-    </Menu>
-  </Container>
+  <Dropdown
+    selection
+    compact
+    text='Sort By'
+    options={OPTIONS}
+    defaultValue={TOP}
+    onChange={(event, data) => props.onChange(data.value)}
+  />
 )
 
 SortBy.PropTypes = {
