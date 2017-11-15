6ac634e06c1e28fd350b507c67cd6013ae9bb4a7
Carlos André <carlos.ulrich@gmail.com>
2017-11-15 12:01:01 +0000

    Test LoadingIcon component

---
+++ frontend/src/components/LoadingIcon/tests/index.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import { Loader } from 'semantic-ui-react'
+import LoadingIcon from 'components/LoadingIcon'
+
+describe('<LoadingIcon />', () => {
+  it('should render a loader', () => {
+    const renderedComponent = shallow(<LoadingIcon />)
+    expect(renderedComponent.type()).toEqual(Loader)
+  })
+
+  it('should have an active attribute', () => {
+    const renderedComponent = shallow(<LoadingIcon />)
+    expect(renderedComponent.prop('active')).toBeDefined()
+  })
+
+  it('should have a size attribute', () => {
+    const renderedComponent = shallow(<LoadingIcon />)
+    expect(renderedComponent.prop('size')).toEqual('large')
+  })
+
+  it('should have a text', () => {
+    const renderedComponent = mount(<LoadingIcon />)
+    expect(renderedComponent.text()).toEqual('Loading...')
+  })
+})
